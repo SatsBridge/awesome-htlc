c6382ec3492d141dca3114a731f513f184fb9c65
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2024-11-27 13:33:25 +0100

    added networks for token deployment

--- evm/misc/token/hardhat.config.ts
+++ evm/misc/token/hardhat.config.ts
@@ -1,6 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
+require("dotenv").config();
+
+const {
+MNEMONIC,
+INFURA_API_KEY,
+LINEASCAN_API_KEY,
+ALCHEMY_API_KEY
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
@@ -9,8 +17,57 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    // Add other networks like Rinkeby, Goerli, etc., here
-  },
+    linea_testnet: {
+      url: `https://linea-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    },
+    arbitrum_testnet: {
+      url: `https://arbitrum-sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    },
+    linea: {
+      url: `https://linea-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    },
+    sepolia: {
+          url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+          accounts: {
+            mnemonic: MNEMONIC,
+            path: "m/44'/60'/0'/0",
+            initialIndex: 0,
+            count: 20,
+            passphrase: "",
+          },
+        },
+    },
 };
 
 export default config;
