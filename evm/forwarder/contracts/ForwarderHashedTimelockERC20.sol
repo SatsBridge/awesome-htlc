1e884c863526b7d1773d3bc75c259eb4a8f915a6
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2024-12-24 16:52:40 +0100

    counterparty input validation for routing

--- evm/forwarder/contracts/ForwarderHashedTimelockERC20.sol
+++ evm/forwarder/contracts/ForwarderHashedTimelockERC20.sol
@@ -65,6 +65,8 @@ contract ForwarderHashedTimelockERC20 is ReentrancyGuard, Ownable {
         address _tokenContract,
         uint256 _amount
     ) external futureTimelock(_timelock) transferable returns (bool) {
+        require(_counterparty != address(0), "Counterparty can't be zero address");
+
         if (_incoming) {
             require(_amount > 0, "Token amount must be > 0");
             require(IERC20(_tokenContract).allowance(_counterparty, address(this)) >= _amount, "Token allowance must be >= amount");
